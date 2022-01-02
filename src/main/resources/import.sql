bbc8e7d6d434f548400b2de2b275cfd840eeb72e
Leandro Portugal <leandroportugal@outlook.com.br>
2022-01-02 14:22:52 -0300

    Entering data into the H test bank

---
+++ src/main/resources/import.sql
@@ -0,0 +1,26 @@
+INSERT INTO beertype(name) VALUES ('American Lager');
+INSERT INTO beertype(name) VALUES ('Pale Ale');
+INSERT INTO beertype(name) VALUES ('IPA');
+INSERT INTO beertype(name) VALUES (' Pilsen');
+INSERT INTO beertype(name) VALUES ('Stout');
+INSERT INTO beertype(name) VALUES ('Vienna Lager');
+INSERT INTO beertype(name) VALUES ('Weiss');
+INSERT INTO beertype(name) VALUES ('Porter');
+INSERT INTO beertype(name) VALUES ('Witbier');
+INSERT INTO beertype(name) VALUES ('Tripel');
+
+
+INSERT INTO beer(beertype_id, name, factory) VALUES (1, 'Skol', 'Ambev');
+INSERT INTO beer(beertype_id, name, factory) VALUES (1, 'Budweiser', 'Anheuser-Busch');
+INSERT INTO beer(beertype_id, name, factory) VALUES (2, 'Heilige', 'Cervejaria Heilige');
+INSERT INTO beer(beertype_id, name, factory) VALUES (3, 'Hop Corn IPA', 'Walls');
+INSERT INTO beer(beertype_id, name, factory) VALUES (4, 'Pilsner Urquell', 'Pilsner Urquell Brewery');
+INSERT INTO beer(beertype_id, name, factory) VALUES (5, 'Oatmeal Stout', 'Cervejaria Imperatriz');
+INSERT INTO beer(beertype_id, name, factory) VALUES (5, 'Guinness Draught', 'St. James Gate Brewery');
+INSERT INTO beer(beertype_id, name, factory) VALUES (6, 'Louvada Vienna', 'Cervejaria Louvada');
+INSERT INTO beer(beertype_id, name, factory) VALUES (7, 'Hefe Weissbier Hell', 'Spaten-Franziskaner-Bräu GmbH');
+INSERT INTO beer(beertype_id, name, factory) VALUES (8, 'London Porter', 'Fullers Brewery');
+INSERT INTO beer(beertype_id, name, factory) VALUES (9, 'Hoegaarden', 'AB InBev');
+INSERT INTO beer(beertype_id, name, factory) VALUES (10, 'St. Feuillien Tripel', 'St. Feuillien');
+
+
